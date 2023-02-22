8baa45eca30c351e5c56b5de514e6675d92edfad
mhshpey <mhshpey@gmail.com>
2023-02-22 20:23:29 +0800

    bug fix

--- gate/from_and_to.cpp
+++ gate/from_and_to.cpp
@@ -71,7 +71,7 @@ std::shared_ptr<Socket> FromAndTo::make_udp_right(std::shared_ptr<Socket> left) 
     return s;
   }
 
-  int index = random() % len;
+  int index = rand() % len;
   s = udpends[index]->make_right();
   if (s) {
     return s;
